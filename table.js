bed0b3e2fe08b47579b6cf0a13bd1d1ad5794e4f
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-05-27 18:00:43 +0700

    Update table.js

--- table.js
+++ table.js
@@ -8,14 +8,18 @@ const db = new sqlite.Database('./app_db.sqlite3', sqlite.OPEN_READWRITE, (err) 
 })
 //Below this line, if not use, comment partial or all of them
 //Create table
-// sql = `CREATE TABLE phone_book(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, number TEXT, is_private INTEGER)`
-// db.run(sql)
-// sql = `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)`
-// db.run(sql)
-// sql = `INSERT INTO users(name, password) VALUES ("Chain13", "IncludedHuman")`
-// db.run(sql)
-// sql = `INSERT INTO phone_book(name, number, is_private) VALUES ("Chain13", "51293487092", 0)`
-// db.run(sql)
-//db.run(sql);
+/*
+sql = `CREATE TABLE phone_book(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, number TEXT, is_private INTEGER)`
+db.run(sql)
+sql = `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)`
+db.run(sql)
+*/
+//After created table uncomment them and comment them later
+/*
+sql = `INSERT INTO users(name, password) VALUES ("Chain13", "IncludedHuman")`
+db.run(sql)
+sql = `INSERT INTO phone_book(name, number, is_private) VALUES ("Chain13", "51293487092", 0)`
+db.run(sql)
+*/
 
 module.exports = db
