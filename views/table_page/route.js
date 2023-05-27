bab45623ef47d83a1199605d70a7c54b343c3520
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-05-27 21:59:22 +0700

    Data (id) is now passable and show nota if there is no data in phone_book

--- views/table_page/route.js
+++ views/table_page/route.js
@@ -6,11 +6,10 @@ let sql
 
 router.get('/table', (req, res) => {
     if (req.session.login) {
-        sql = `SELECT name, owner, number FROM phone_book`
+        sql = `SELECT id, name, owner, number FROM phone_book`
         db.all(sql, [], (err, rows) => {
             if (err) return console.log(err.message)
-            console.log(rows)
-            res.render('./table_page/templates/index.ejs', { data: rows})
+            res.render('./table_page/templates/index.ejs', { data: rows, user: req.session.username})
         })
         //res.sendFile(path.join(__dirname, '/templates/index.html'))
     }
@@ -21,19 +20,22 @@ router.get('/table', (req, res) => {
 router.post('/addUser', (req, res) => {
     const name = req.body.name
     const phone = req.body.phoneNumber
-    sql = "INSERT INTO phone_book (name, number) VALUES (?,?)"
+    sql = "INSERT INTO phone_book (name, owner, number) VALUES (?,?,?)"
     console.log(name, phone)
 })
 router.post('/editUser', (req,res)=> {
     const name = req.body.name
     const phone = req.body.phoneNumber
-    sql = "UPDATE phone_book SET number = ? WHERE id = ?"
+    sql = "UPDATE phone_book SET name = ?, number = ? WHERE id = ?"
     console.log(name, phone)
 })
 router.post('/deleteUser', (req, res)=> {
     const id = req.body.id
     sql = `DELETE FROM phone_book WHERE id=?`
-    console.log(id)
+    db.run(sql, [id], (err) => {
+        if (err) return console.log(err.message)
+    })
+    console.log("This run!")
     res.redirect('/')
 })
 module.exports = router
