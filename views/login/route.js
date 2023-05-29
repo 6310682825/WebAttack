b591a84366ad943e3b3cf378ff81bebb0d16fcc2
Chaiwat <Chaiwat@cn351>
2023-05-30 00:00:30 +0700

    weak to sqli

--- views/login/route.js
+++ views/login/route.js
@@ -20,8 +20,8 @@ router.post('/login', (req, res) => {
     const username = req.body.username
     const password = req.body.password
     const expire = 30000
-    sql = `SELECT * FROM users WHERE name = ? AND password = ?;`
-    db.all(sql,[username, password],(err,rows)=>{
+    sql = `SELECT * FROM users WHERE name = "${username}" AND password = "${password}";`
+    db.all(sql,[],(err,rows)=>{
         if (err) return res.redirect('/login')
         if (rows.length == 0) return res.redirect('/login')
         req.session.username = username
@@ -30,5 +30,17 @@ router.post('/login', (req, res) => {
         req.session.cookie.maxAge = expire
         res.redirect('/')
     })
+    /*
+    if (username === "Chain13" && password === "IncludedHuman") {
+        req.session.username = username
+        req.session.password = password
+        req.session.login = true
+        req.session.cookie.maxAge = expire
+        res.redirect('/')
+    }
+    else {
+        res.redirect('/login')
+    }
+    */
 })
 module.exports = router
