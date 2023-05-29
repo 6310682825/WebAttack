27f8956cf2c4a12ddf7254f74961aa16bd832751
Chaiwat <Chaiwat@cn351>
2023-05-29 20:50:41 +0700

    update table.js

--- table.js
+++ table.js
@@ -8,18 +8,24 @@ const db = new sqlite.Database('./app_db.sqlite3', sqlite.OPEN_READWRITE, (err) 
 })
 //Below this line, if not use, comment partial or all of them
 //Create table
-/*
-sql = `CREATE TABLE phone_book(id INTEGER PRIMARY KEY AUTOINCREMENT, owner TEXT, name TEXT , number TEXT, is_private INTEGER)`
-db.run(sql)
-sql = `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)`
-db.run(sql)
-*/
+
+// sql = `CREATE TABLE phone_book(id INTEGER PRIMARY KEY AUTOINCREMENT, owner TEXT, name TEXT , number TEXT, is_private INTEGER)`
+// db.run(sql)
+// sql = `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)`
+// db.run(sql)
+
 //After created table uncomment them and comment them later
-/*
-sql = `INSERT INTO users(name, password) VALUES ("Chain13", "IncludedHuman")`
-db.run(sql)
-sql = `INSERT INTO phone_book(owner, name, number, is_private) VALUES ("Chain13", "Computer shop", "51293487092", 0)`
-db.run(sql)
-*/
+
+// sql = `INSERT INTO users(name, password) VALUES ("Chain13", "IncludedHuman")`
+// db.run(sql)
+// sql = `INSERT INTO users(name, password) VALUES ("Homunculus", "NotIncludedHuman")`
+// db.run(sql)
+// sql = `INSERT INTO phone_book(owner, name, number, is_private) VALUES ("Chain13", "Computer shop", "51293487092", 1)`
+// db.run(sql)
+// sql = `INSERT INTO phone_book(owner, name, number, is_private) VALUES ("Homunculus", "Car shop", "1412789", 1)`
+// db.run(sql)
+// sql = `INSERT INTO phone_book(owner, name, number, is_private) VALUES ("Homunculus", "Tree shop", "75395", 0)`
+// db.run(sql)
+
 
 module.exports = db
